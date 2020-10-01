bae44ad9fd658ee3294f9e55faf0d9bfa1f3075b
yoerivd <yoeri.vandamme@infosupport.com>
2020-10-01 16:58:09 +0200

    comments

--- generics-constraints/IRepository.cs
+++ generics-constraints/IRepository.cs
@@ -2,16 +2,22 @@ using System.Collections.Generic;
 
 namespace generics_constraints
 {
+    /// <summary>
+    ///     contravariant: methods may take derived types of T as a parameter
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     internal interface IWriteOnlyRepository<in T> // contravariant: methods may take more derived objects as parameter
     {
         void Add(T entity);
         void Delete(T entity);
     }
-/// <summary>
-/// covariant: methods may return derived objects eg: IRepository<Student> == IReadOnlyRepository<Person> because all Students are Persons
-/// </summary>
-/// <typeparam name="T"></typeparam>
-    internal interface IReadOnlyRepository<out T> 
+
+    /// <summary>
+    ///     covariant: methods may return derived types of T eg: IRepository
+    ///     <Student> == IReadOnlyRepository<Person> because all Students are Persons
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal interface IReadOnlyRepository<out T>
     {
         T Find(int id);
         IEnumerable<T> GetAll();
