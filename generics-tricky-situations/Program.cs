bf14c0e556cf2696598690b61e30603b41c16e86
yoerivd <yoeri.vandamme@infosupport.com>
2020-10-01 18:14:53 +0200

    add tricky situation

---
+++ generics-tricky-situations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace generics_tricky_situations
+{
+    internal class MyItem<T>
+    {
+        public static int InstanceCount { get; private set; }
+
+        public MyItem()
+        {
+            InstanceCount++;
+        }
+    }
+    internal class Person{}
+    internal class Teacher : Person{}
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            new MyItem<int>();
+            new MyItem<int>();
+            
+            new MyItem<string>();
+            
+            Console.WriteLine(MyItem<int>.InstanceCount);
+            Console.WriteLine(MyItem<string>.InstanceCount);
+            
+            new MyItem<Person>();
+            new MyItem<Person>();
+            new MyItem<Teacher>();
+            
+            Console.WriteLine(MyItem<Person>.InstanceCount);
+            Console.WriteLine(MyItem<Teacher>.InstanceCount);
+        }
+    }
+}
