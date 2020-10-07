cc693f6a7a5c92f70073590b57ea935b658f07ee
yoerivd <yoeri.vandamme@infosupport.com>
2020-10-07 17:28:13 +0200

    remove unused demos

--- WpfApp.Server/Message.cs
+++
@@ -1,9 +0,0 @@
-namespace WpfApp.Server
-{
-    public class Message
-    {
-        public string? Sender { get; set; }
-        public string? Title { get; set; }
-        public string? Content { get; set; }
-    }
-}
